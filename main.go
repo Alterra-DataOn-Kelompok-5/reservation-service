7e131a0714e5de52df4c6cff81425da896fd5dc9
Samuel Christopher <samuel.christopher.santo@gmail.com>
2022-07-08 17:11:23 +0700

    feat: add reservation handler

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/Alterra-DataOn-Kelompok-5/reservation-service/database/migration"
 	"github.com/Alterra-DataOn-Kelompok-5/reservation-service/middleware"
 
+	_reservationHttp "github.com/Alterra-DataOn-Kelompok-5/reservation-service/reservation/delivery/http"
+	_reservationRepo "github.com/Alterra-DataOn-Kelompok-5/reservation-service/reservation/repository"
+	_reservationUc "github.com/Alterra-DataOn-Kelompok-5/reservation-service/reservation/usecase"
+
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
@@ -27,6 +31,10 @@ func main() {
 	e := echo.New()
 
 	middleware.Init(e)
+
+	reservationRepo := _reservationRepo.NewMysqlReservationsRepository(database.GetConnection())
+	rvu := _reservationUc.NewreservationUsecase(reservationRepo)
+	_reservationHttp.NewReservationsHandler(e, rvu)
 
 	log.Fatal(e.Start(":" + os.Getenv("SERVICE_PORT")))
 }
