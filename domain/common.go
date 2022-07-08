502eeca638e663e4db3100da90b00fb18b0c9a42
Samuel Christopher <samuel.christopher.santo@gmail.com>
2022-07-08 16:24:31 +0700

    fix: revamp table reservations & reservation_details

--- domain/common.go
+++ domain/common.go
@@ -2,13 +2,11 @@ package domain
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Model struct {
 	ID        uint `gorm:"primaryKey;autoIncrement"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	// DeletedAt gorm.DeletedAt `gorm:"index"`
 }
